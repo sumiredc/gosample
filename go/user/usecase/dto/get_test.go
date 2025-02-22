996ffb8feb445c3a14bcc7188fd2604f9c6f1f33
Ryosuke Sakamoto <sakamoto@sumire-sakamoto.co.jp>
2025-02-22 21:20:51 +0900

    test: Add test case of GetUserInput and GetUserOutput

---
+++ go/user/usecase/dto/get_test.go
@@ -0,0 +1,34 @@
+package dto
+
+import (
+	"sample/user/domain/entity"
+	"sample/user/domain/valueobject"
+	"testing"
+)
+
+func TestGetUserInput(t *testing.T) {
+	userID := "01JMPR93HT4GN3GDBMFP91C8J9"
+
+	t.Run("should initialize GetUserInput with expected values", func(t *testing.T) {
+		i := NewGetUserInput(userID)
+
+		if i.UserID() != userID {
+			t.Errorf("UserID missmatch: expected %q, but got %q", userID, i.UserID())
+		}
+	})
+}
+
+func TestGetUserOutput(t *testing.T) {
+	userID := valueobject.NewUserID()
+	name := "test name"
+	email := "test@test.xxx"
+	user := entity.NewUser(userID, name, email)
+
+	t.Run("should initialize GetUserOutput with expected values", func(t *testing.T) {
+		o := NewGetUserOutput(user)
+
+		if o.user.ID.String() != userID.String() {
+			t.Errorf("UserID missmatch: expected %q, but got %q", userID.String(), o.user.ID.String())
+		}
+	})
+}
