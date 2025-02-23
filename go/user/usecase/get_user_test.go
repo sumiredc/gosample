e473570023f4a70f788d87ed5433609243a843ca
Ryosuke Sakamoto <sakamoto@sumire-sakamoto.co.jp>
2025-02-23 13:46:15 +0900

    test: Add test case of GetUserUseCase

---
+++ go/user/usecase/get_user_test.go
@@ -0,0 +1,56 @@
+package usecase_test
+
+import (
+	"errors"
+	"sample/user/domain/entity"
+	"sample/user/domain/valueobject"
+	"sample/user/internal/mockrepository"
+	"sample/user/usecase"
+	"sample/user/usecase/dto"
+	"testing"
+)
+
+func TestGetUserUseCase(t *testing.T) {
+	t.Run("should get a user successfully", func(t *testing.T) {
+		userID := valueobject.NewUserID()
+		name := "test name"
+		email := "test@test.xxx"
+		eUser := entity.NewUser(userID, name, email)
+
+		mRepo := new(mockrepository.MockUserRepository)
+		mRepo.On("Get", userID).Return(eUser, nil)
+
+		i := dto.NewGetUserInput(userID.String())
+		u := usecase.NewGetUserUseCase(mRepo)
+		o, err := u.Execute(i)
+
+		if err != nil {
+			t.Errorf("failed to get a user: %v", err)
+		}
+
+		user := o.User()
+
+		if user.ID.String() != userID.String() {
+			t.Errorf("UserID missmatch: expected %q, but got %q", userID.String(), user.ID.String())
+		}
+
+		mRepo.AssertNumberOfCalls(t, "Get", 1)
+	})
+
+	t.Run("should return to Error", func(t *testing.T) {
+		userID := valueobject.NewUserID()
+
+		mRepo := new(mockrepository.MockUserRepository)
+		mRepo.On("Get", userID).Return(nil, errors.New("Failed to get a user"))
+
+		i := dto.NewGetUserInput(userID.String())
+		u := usecase.NewGetUserUseCase(mRepo)
+		_, err := u.Execute(i)
+
+		if err == nil {
+			t.Errorf("failed to return not error")
+		}
+
+		mRepo.AssertNumberOfCalls(t, "Get", 1)
+	})
+}
